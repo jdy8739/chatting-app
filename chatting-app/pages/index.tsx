53d485083bdb089027c435e3e79121a5f69310f8
jdy8739 <jdy8739@naver.com>
2022-07-05 11:50:38 +0900

    시작 세팅

---
+++ chatting-app/pages/index.tsx
@@ -0,0 +1,12 @@
+import type { NextPage } from 'next'
+import Seo from '../components/Seo'
+
+const Home: NextPage = () => {
+  return (
+    <div>
+      <Seo title={'Hello Chato ;)'}></Seo>
+    </div>
+  )
+}
+
+export default Home
