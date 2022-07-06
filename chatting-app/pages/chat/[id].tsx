b1c901d4e29c1ece6e3928be32eca075505f292c
jdy8739 <jdy8739@naver.com>
2022-07-06 16:01:19 +0900

    채팅창 스타일

--- chatting-app/pages/chat/[id].tsx
+++ chatting-app/pages/chat/[id].tsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useRef, useState } from "react";
 import webstomp from "webstomp-client";
 import Seo from "../../components/Seo";
+import { generateRandonUserId } from "../../utils/utils";
 
 interface IMessageBody {
     roomId: string,
@@ -10,6 +11,7 @@ interface IMessageBody {
 
 let socket: WebSocket;
 let stomp: any;
+let randonUserId: string = '';
 
 function ChattingRoom({ id }: { id: number }) {
     let newMessage: string;
@@ -17,13 +19,14 @@ function ChattingRoom({ id }: { id: number }) {
     const formRef = useRef<HTMLInputElement>(null);
     const handleChatSubmit = (e: React.FormEvent<HTMLFormElement>) => {
         e.preventDefault();
+        if (formRef.current?.value === '') return;
         if (formRef.current) {
             newMessage = formRef.current.value;
             formRef.current.value = '';
         }
         if (socket && stomp) {
             stomp.send('/pub/chat/message', JSON.stringify({
-                roomId: id, message: newMessage, writer: 'jdy8739' }));
+                roomId: id, message: newMessage, writer: randonUserId }));
         }
     };
     const updateMessageList = (newMessageInfo: IMessageBody) => {
@@ -32,24 +35,41 @@ function ChattingRoom({ id }: { id: number }) {
             copied.push(newMessageInfo);
             return copied;
         });
-    }
+    };
     const subScribeNewMessage = () => {
         stomp.subscribe(`/sub/chat/room/${id}`, ({ body }: { body: string }) => {
             const newMessage: IMessageBody = JSON.parse(body);
             updateMessageList(newMessage);
+            window.scrollTo(0, document.body.scrollHeight);
         })
     };
     useEffect(() => {
         socket = new WebSocket('ws://localhost:5000/stomp/chat');
         stomp = webstomp.over(socket);
         stomp.connect({}, subScribeNewMessage);
-    }, [ ]);
+        randonUserId = generateRandonUserId();
+        return () => {
+            randonUserId = '';
+        }
+    }, []);
     return (
         <>
             <Seo title={`Chato room ${id}`}/>
-            {
-                messages.map((msg, i) => <p key={i}>{msg.message}</p>)
-            }
+            <div className="container">
+                {
+                    messages.map((msg, i) => 
+                        <div key={i} 
+                            className={`chat-box ${msg.writer === randonUserId ? 'my-chat-box' : 'others-chat-box'}`}
+                        >
+                            <span 
+                                className={`chat ${msg.writer === randonUserId ? 'my-chat' : 'others-chat'}`}
+                            >
+                                {`${msg.writer.slice(0, 9)} ${msg.message}`}
+                            </span>
+                        </div>
+                    )
+                }
+            </div>
             <form onSubmit={handleChatSubmit}>
                 <input ref={formRef} />
                 <button>submit</button>
